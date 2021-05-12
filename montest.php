eaa0a3276425e438087c0a6216153eacccf384eb
Allin RAMANAMPISOA <Aallin13ramanampisoa@outlook.fr>
2021-05-12 13:05:10 +0300

    Edit d'un test

--- montest.php
+++ montest.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Test";
+$servername = "echo.cloudns.io";
+$username = "ibcmgcom_magasinibc_user";
+$password = "ibc2020@IBC";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
